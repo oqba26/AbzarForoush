4777cc16f61f58b0b98cc88ae5165fbbe7fee598
oqba26 <oqba26@gmail.com>
2026-07-14 08:08:02 +0330

    باز طراحی دیالوگ انتخاب تاریخ شمسی ، اصلاح موارد جزئی دیگر

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.oqba26.abzarforoush"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
+        versionCode = 18
         versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
